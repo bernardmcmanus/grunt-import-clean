0a1fdc49920b900c027aa85f6fd48678cfaab06b
Dave Williams <davnicwil@gmail.com>
2015-12-06 16:37:26 -0500

    Add option to ignore specific named 'unused' imports.
    If ignored imports are found, they are reported with a warning but grunt will continue.
    
    Provides finer-grain control than using the force: true option, which basically ignores all unused imports. It allows you to ignore certain unused imports which you know are valid (for example the 'react' import in JSX files, where the import is not referenced in the JSX code but will be referenced in the transpiled js code, and is therefore required) but break the build on others.

--- tasks/lib/aggregate.js
+++ tasks/lib/aggregate.js
@@ -4,20 +4,35 @@ module.exports = (function() {
 
   var path = require( 'path' );
 
-  return function( files ) {
+  return function( files, importsToIgnore ) {
+
     var result = {
       unused: {},
       totalFiles: files.length,
       foundFiles: 0,
-      foundImports: 0
+      foundImports: 0,
+      ignoredImports: 0
+    };
+
+    var isIgnoredImport = function( unused ) {
+      return importsToIgnore.indexOf(unused) != -1;
+    };
+
+    var reportUnusedImport = function( unused ) {
+      if (isIgnoredImport( unused )) {
+        result.ignoredImports++;
+        return unused + '   (IGNORED)';
+      } else {
+        result.foundImports++;
+        return unused;
+      }
     };
 
     files.forEach(function( file ) {
       var name = path.basename( file.src );
       if (file.unused.length) {
-        result.unused[name] = file.unused;
+        result.unused[name] = file.unused.map(reportUnusedImport);
         result.foundFiles++;
-        result.foundImports += file.unused.length;
       }
     });
 
